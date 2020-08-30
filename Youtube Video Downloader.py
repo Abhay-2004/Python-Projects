6367dbdd01a0bf62557f4dc29bf9a905f2f01880
Abhay-2004 <70215568+Abhay-2004@users.noreply.github.com>
2020-08-30 21:34:34 +0530

    Update Youtube Video Downloader.py

--- Youtube Video Downloader.py
+++ Youtube Video Downloader.py
@@ -4,6 +4,9 @@ link = input("Enter the link here: ")
 yt_l = YouTube(link)
 
 print("Title:",yt_l.title)
+print("Title:",yt_l.views)
+print("Title:",yt_l.rating)
+print("Title:",yt_l.length)
 
 resolution = yt_l.streams.get_highest_resolution()
 
